acf3b5d7c389348e703f5c8bcfcb154fe7c00a32
shafayatGit <shafayathossain.drmc@gmail.com>
2025-02-07 21:39:55 +0600

    ternary added

---
+++ shorthand.js
@@ -0,0 +1,4 @@
+var price = 30;
+// price = price + 10;
+price +=10;
+console.log(price)
---
+++ ternary.js
@@ -0,0 +1,47 @@
+// Ternary ==> condition ? console log if true : console log if false
+
+let age = 19;
+
+ age>18?console.log("can vote"):console.log("cant vote")
+
+ let price = 500;
+ let isLeader = true;
+ 
+//  if(isLeader===true){
+//     price = price * 0.5;
+    
+//  }
+//  else(
+//     price= price + 200
+    
+//  )
+//  console.log(price)
+
+// Ternary Form
+
+ isLeader===true ? price = price *0.5 : price = price + 200;
+ console.log(price)
+
+
+
+ let taka = 1200;
+ isDalal = true;
+
+//  if(isDalal== true){
+//     if(taka > 1000){
+//         taka = taka * 0.5
+//     }
+//     else{
+//         taka = 0;
+//     }
+//  }
+//  else{
+//     taka = taka;
+//  }
+//  console.log(taka);
+
+//  Ternary form
+
+ isDalal === true? taka>1000 ? taka=taka*0.5 : taka= 0 : taka = taka;
+  console.log(taka)
+
