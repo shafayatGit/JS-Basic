6d645cf0b6778b42fb32dd6c2a5651add3c1e5c4
shafayatGit <shafayathossain.drmc@gmail.com>
2025-02-21 23:31:53 +0600

    push

---
+++ Object JS/nastedObj.js
@@ -0,0 +1,19 @@
+let nayok = {
+    name: 'Sakib Khan',
+    id : 121,
+    address : 'movie',
+    isSingle: true,
+    friends: ['Apu', 'Riyaz', 'Bipasha'],
+    car: {
+        brand: 'Tesla',
+        price: 500000000,
+        made: 2025, 
+    } ,
+
+    act: function(){
+        console.log('Acting like sakib khan')
+    } 
+}
+
+console.log(nayok.car.brand)
+nayok.act()  //function just call kore dibo..console kora lagbe na
